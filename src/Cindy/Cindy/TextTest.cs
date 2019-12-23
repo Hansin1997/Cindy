1d3274918b8929726e45f2f83d7e8210364cc9e9
Hansin1997 <845612500@qq.com>
2019-12-23 15:03:36 +0800

    replace libary

---
+++ src/Cindy/Cindy/TextTest.cs
@@ -0,0 +1,25 @@
+﻿using Cindy.Strings;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cindy
+{
+    public class TextTest : MonoBehaviour
+    {
+        public Text text;
+        [Multiline]
+        public string key;
+        public StringSource stringSource;
+
+
+
+        protected void Update()
+        {
+            if(stringSource != null && text != null && key != null)
+            {
+                text.text = stringSource.GetString(key, key);
+            }
+        }
+
+    } 
+}
